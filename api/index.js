838b6cb177455804fbc846926a66b36b97cf35a8
Timothy Ohagan <tmohagan@gmail.com>
2024-06-03 14:11:27 -0400

    api deployment

--- api/index.js
+++ api/index.js
@@ -7,7 +7,6 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
 const multer = require('multer');
-// /tmp is a Vercel directory we can use to upload files to be uploaded to aws
 const uploadMiddleware = multer({ dest: '/tmp' });
 const {S3Client, PutObjectCommand} = require('@aws-sdk/client-s3');
 const fs = require('fs');
@@ -24,19 +23,6 @@ app.use(express.json());
 app.use(cookieParser());
 app.use('/uploads', express.static(__dirname + '/uploads'));
 
-/* 
-on Vercel every endpoint will be a seperate function run on aws lambda
-so instead of connecting to the db here, connect instead every endpoint
-mongoose.connect(process.env.MONGO_URL);
-*/
-
-/*
-will need the aws-sdk
-yarn add @aws-sdk/client-s3
-
-path is the location of the file after uploaded to Vercel tmp directory
-uploading the file from Vercel to aws
-*/
 async function uploadToS3(path, originalFilename, mimetype) {
   const client = new S3Client({
     region: 'us-east-2',
@@ -57,6 +43,11 @@ async function uploadToS3(path, originalFilename, mimetype) {
   }));
   return `https://${bucket}.s3.amazonaws.com/${newFilename}`;
 }
+
+app.get('/test', (req,res) => {
+  res.json('ok');
+}
+);
 
 app.post('/register', async (req,res) => {
   mongoose.connect(process.env.MONGO_URL);
@@ -93,7 +84,6 @@ app.post('/login', async (req,res) => {
 });
 
 app.get('/profile', (req,res) => {
-  // don't think we need to connect to the database for this
   mongoose.connect(process.env.MONGO_URL);
   const {token} = req.cookies;
   jwt.verify(token, secret, {}, (err,info) => {
@@ -106,23 +96,6 @@ app.post('/logout', (req,res) => {
   res.cookie('token', '').json('ok');
 });
 
-/*
-need to upload to aws with s3 client instead of local with multer
-
-path is the path for the file after it has been uploaded to the Vercel (should be in tmp directory)
-  note: it is renamed and does not include extention
-originalname is used to get the extension
-mimetype is the file type (e.g. image/jpeg)
-
-instead of using multer to upload the files to our local machine
-  and then taking the new path to where multer saved
-  and saving that path in the database
-
-  use multer to upload the file to the "local" Vercel host
-    (Vercel is a remote host, but since that is where the app is running, it's local to where it's running)
-    then we use the Vercel file path to upload that file from Vercel to AWS
-    then save the path the the file on AWS in the Database
-*/
 app.post('/post', uploadMiddleware.single('file'), async (req,res) => {
   mongoose.connect(process.env.MONGO_URL);
   const {originalname, path, mimetype} = req.file;
@@ -143,9 +116,6 @@ app.post('/post', uploadMiddleware.single('file'), async (req,res) => {
   });
 });
 
-/*
-need to upload to s3 instead of uploads
-*/
 app.put('/post',uploadMiddleware.single('file'), async (req,res) => {
   mongoose.connect(process.env.MONGO_URL);
   let imageUrl = null;
