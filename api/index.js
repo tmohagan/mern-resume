68f696bc43e702dfd353c03f91538bcbfcef0aa3
Timothy Ohagan <tmohagan@gmail.com>
2024-07-23 13:15:04 -0400

    backend cors

--- api/index.js
+++ api/index.js
@@ -33,7 +33,7 @@ const allowedOrigins = [
 ];
 
 const corsOptions = {
-  origin: (origin, callback) => {
+  origin: function (origin, callback) {
     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
       callback(null, true);
     } else {
