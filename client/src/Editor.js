03cf2cd7ca9c705c65b64fd1d815067cea144755
Timothy Ohagan <tmohagan@gmail.com>
2024-07-27 16:41:30 -0400

    fix editor

--- client/src/Editor.js
+++ client/src/Editor.js
@@ -1,27 +1,25 @@
+// Editor.js
 import ReactQuill from "react-quill";
+import 'react-quill/dist/quill.snow.css';
 
 export default function Editor({value,onChange}) {
   const modules = {
     toolbar: [
       [{ header: [1, 2, false] }],
       ['bold', 'italic', 'underline', 'strike', 'blockquote'],
-      [
-        { list: 'ordered' },
-        { list: 'bullet' },
-        { indent: '-1' },
-        { indent: '+1' },
-      ],
+      [{ list: 'ordered' }, { list: 'bullet' }, { indent: '-1' }, { indent: '+1' }],
       ['link', 'image'],
       ['clean'],
     ],
   };
+
   return (
-    <div className="content">
     <ReactQuill
       value={value}
-      theme={'snow'}
       onChange={onChange}
-      modules={modules} />
-    </div>
+      modules={modules}
+      theme="snow"
+      style={{marginBottom: '20px'}}
+    />
   );
 }
